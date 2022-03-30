aa628351c65d6521e93b6a45e64258ed3d2e5fb2
IrvingSanz <irvingsanchez@sonorasoft.com>
2022-03-29 18:02:55 -0700

    Add project files.

---
+++ AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using giftShop4.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giftShop4
+{
+    
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace giftShop4.Models
+{
+    public class Users
+    {
+        [Key]
+        public int IdUser { get; set; }
+        [Required]
+        public string Name{ get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public DateTime BirthDay { get; set; }
+    }
+}
